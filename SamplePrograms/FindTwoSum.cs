3c9fe25c95da4f19627e6036129c079dc9d7f32e
harishthorati <harish.thorati@outlook.com>
2016-07-09 12:29:01 -0500

    Initial Set up for TestDome  C# solutions

---
+++ SamplePrograms/FindTwoSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplePrograms
+{
+    class FindTwoSum
+    {
+        public static Tuple<int, int> TwoSum(IList<int> list, int sum)
+        {
+            return new Tuple<int, int>(list[0], sum);
+        }
+
+        public static void Main(string[] args)
+        {
+            Tuple<int, int> indices = TwoSum(new List<int>() { 1, 3, 5, 7, 9 }, 12);
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+        }
+    }
+}
